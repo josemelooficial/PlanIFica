e72448e5e625560a311c80c98892f687a70f0007
JorgeDomingos068 <domingosjorge068@gmail.com>
2025-09-09 16:38:03 -0400

    removendo cache e linhas desnecessárias

--- app/Controllers/AulaHorarioController.php
+++ app/Controllers/AulaHorarioController.php
@@ -4,28 +4,13 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
-use App\Models\CursosModel;
-use App\Models\VersoesModel;
-use App\Models\AmbientesModel;
 use App\Models\AulaHorarioModel;
-use App\Models\AulaHorarioAmbienteModel;
-use CodeIgniter\HTTP\Request;
-use CodeIgniter\HTTP\ResponseInterface;
-use CodeIgniter\Cache\CacheInterface;
-
 
 class AulaHorarioController extends BaseController
 {
   public function verificarConflitosRotina() {
 
-    $cache = cache(); /** @var CacheInterface $cache */
-
     $versaoId = (new \App\Models\VersoesModel())->getVersaoByUser(auth()->id());
-
-    $cacheKey = "rotina_conflitos{$versaoId}";
-    if ($conflitos = $cache->get($cacheKey)) {
-        return $this->response->setJSON($conflitos);
-    }
 
     $model = new \App\Models\AulaHorarioModel();
     $amb  = $model->countConflitosAmbiente($versaoId);
@@ -39,8 +24,6 @@ class AulaHorarioController extends BaseController
       'RESTRIÇÃO-DOCENTE' => $restricao,
       'CONFLITO-TURNOS' => $turnos,
     ];
-
-    $cache->save($cacheKey, $conflitos, 60);
 
     return $this->response->setJSON($conflitos);
   }
