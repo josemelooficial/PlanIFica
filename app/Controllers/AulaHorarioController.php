d48f43cd97cbb6bba228bcc077825acc538efc84
JorgeDomingos068 <domingosjorge068@gmail.com>
2025-09-09 21:06:26 -0400

    refazendo consulta de conflito de intervalos

--- app/Controllers/AulaHorarioController.php
+++ app/Controllers/AulaHorarioController.php
@@ -13,16 +13,19 @@ class AulaHorarioController extends BaseController
     $versaoId = (new \App\Models\VersoesModel())->getVersaoByUser(auth()->id());
 
     $model = new \App\Models\AulaHorarioModel();
+    
     $amb  = $model->countConflitosAmbiente($versaoId);
     $prof = $model->countConflitosProfessor($versaoId);
     $restricao = $model->countRestricaoDocente($versaoId);
     $turnos = $model->countTresTurnos($versaoId);
+    $intervalo = $model->countTempoEntreTurnos($versaoId);
 
     $conflitos = [
       'CONFLITO-AMBIENTE' => $amb,
       'CONFLITO-PROFESSOR' => $prof,
       'RESTRIÇÃO-DOCENTE' => $restricao,
       'CONFLITO-TURNOS' => $turnos,
+      'CONFLITO-INTERVALO' => $intervalo,
     ];
 
     return $this->response->setJSON($conflitos);
